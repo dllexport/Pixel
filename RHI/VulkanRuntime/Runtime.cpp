f9650da843957d35c01e5c4fdedc8e567514ba65
Mario Lau <mario@dllexport.com>
2023-08-23 00:31:54 +0800

    set perfered sc format VK_FORMAT_B8G8R8A8_UNORM

--- RHI/VulkanRuntime/Runtime.cpp
+++ RHI/VulkanRuntime/Runtime.cpp
@@ -110,7 +110,7 @@ IntrusivePtr<SwapChain> VulkanRuntime::CreateSwapChain(void *handle, uint32_t wi
         .SetExtent(width, height)
         .SetHandle(handle)
         .SetPreferPresentMode(VK_PRESENT_MODE_FIFO_KHR)
-        .SetPreferFormat({VK_FORMAT_B8G8R8A8_SRGB, VK_COLOR_SPACE_SRGB_NONLINEAR_KHR})
+        .SetPreferFormat({VK_FORMAT_B8G8R8A8_UNORM})
         .Build();
 }
 

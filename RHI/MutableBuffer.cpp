5614ec36e318aedebc70fb87dc9d451ee770051f
Mario Lau <mario@dllexport.com>
2023-09-07 00:13:51 +0800

    mb with setter

--- RHI/MutableBuffer.cpp
+++ RHI/MutableBuffer.cpp
@@ -5,19 +5,25 @@ MutableBuffer::MutableBuffer(IntrusivePtr<Buffer> defaultBuffer) : ResourceHandl
 void MutableBuffer::ReSize(uint32_t size)
 {
     int currentSize = buffers.size();
-    if (currentSize > size)
+    if (currentSize >= size)
     {
-        buffers.resize(size);
         return;
     }
 
-    for (int i = currentSize; i <= size; i++)
+    for (int i = currentSize; i < size; i++)
     {
         buffers.push_back(buffers[0]->Clone());
     }
 }
 
-IntrusivePtr<Buffer> &MutableBuffer::GetBuffer(uint32_t index)
+IntrusivePtr<Buffer>& MutableBuffer::GetBuffer(uint32_t index)
 {
+    ReSize(index + 1);
     return buffers[index];
+}
+
+void MutableBuffer::SetBuffer(uint32_t index, IntrusivePtr<Buffer> buffer)
+{
+    ReSize(index + 1);
+    buffers[index] = buffer;
 }
--- RHI/MutableBuffer.h
+++ RHI/MutableBuffer.h
@@ -11,8 +11,9 @@ class MutableBuffer : public ResourceHandle
 public:
     MutableBuffer(IntrusivePtr<Buffer> defaultBuffer);
     void ReSize(uint32_t size);
-    IntrusivePtr<Buffer> &GetBuffer(uint32_t index);
-
+    IntrusivePtr<Buffer>& GetBuffer(uint32_t index);
+    void SetBuffer(uint32_t index, IntrusivePtr<Buffer> buffer);
+    
 protected:
     virtual ~MutableBuffer() = default;
 
